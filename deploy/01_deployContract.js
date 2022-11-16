e76474b9efc34e11ff502e2bf454487f8594a3b3
Muhtasim Fahim <muhtasim.fahim.98@gmail.com>
2022-11-16 18:57:37 +0600

    escrow-contract-deployment-fixed

--- deploy/01_deployContract.js
+++ deploy/01_deployContract.js
@@ -2,11 +2,7 @@
 /* eslint-disable node/no-unpublished-require */
 const { network } = require("hardhat");
 const { ethers } = require("hardhat");
-const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), "ether");
-};
 
-const ether = tokens;
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
@@ -19,6 +15,16 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const Escrow = await deploy("Escrow", {
     from: deployer,
     log: true,
+    args: [
+      RealEstate.address,
+      1,
+      ethers.utils.parseUnits("10", "gwei"),
+      ethers.utils.parseUnits("2", "gwei"),
+      "0xb9884b3aB614E60ED29f6d803b85255c8D46b91d",
+      "0xb9884b3aB614E60ED29f6d803b85255c8D46b91d",
+      "0xb9884b3aB614E60ED29f6d803b85255c8D46b91d",
+      "0xb9884b3aB614E60ED29f6d803b85255c8D46b91d",
+    ],
   });
 };
 
